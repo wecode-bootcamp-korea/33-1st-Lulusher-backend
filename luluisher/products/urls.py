5442fb17148219ffc9beb58252e7f6b45f641ad3
DDinoBox <dinobox.create@gmil.com>
2022-05-31 07:41:33 +0900

    menu nav 기능 추가
    menu nav 기능 추가
    메뉴, 메인카테고리, 서브카테고리 확인

--- luluisher/products/urls.py
+++ luluisher/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from products.views import productListView
+from products.views import productListView, MenuListView
 
 urlpatterns = [
     path('/productList', productListView.as_view()),
+    path('/menu', MenuListView.as_view()),
     ]
--- luluisher/products/views.py
+++ luluisher/products/views.py
@@ -69,10 +69,27 @@ class productListView(View):
                     'option_price'         : str(int(product_options.option_price)+int(product.price)),
                     'product_option_images': [i.image_url for i in ProductOptionImage.objects.filter(product_option_id=product_options.id)]
                     } for product_options in ProductOption.objects.filter(product_id=product.id)]
-                }for product in products
+                } for product in products
             ]
 
             return JsonResponse({"content" : product_list}, status = 200)
 
         except Menu.DoesNotExist:
             return JsonResponse({"message" : "MENU_DOES_NOT_EXIST"}, status = 400)
+
+class MenuListView(View) :
+    def get(self, request) :
+        category_list = [{
+            'id'           : menu.id,
+            'name'         : menu.name,
+            'main_category': [{
+                'id'          : main_category.id,
+                'name'        : main_category.name,
+                'sub_category': [{
+                    'id'  : sub_category.id,
+                    'name': sub_category.name
+                    } for sub_category in main_category.subcategory_set.all()]
+                } for main_category in menu.maincategory_set.all()]
+            } for menu in Menu.objects.all().order_by('id')]
+        
+        return JsonResponse({'category_list':category_list}, status = 200)
