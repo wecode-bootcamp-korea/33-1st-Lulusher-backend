d4a6f17cfa39670649ecfc89f1dfc9c3ea0c78de
Mia Lee <mia@Miaui-MacBookPro.local>
2022-05-30 18:23:57 +0900

    products app - review modeling 수정 및 TimeStampModel 참조

---
+++ luluisher/products/views.py
@@ -0,0 +1,36 @@
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
+
+from products.models import Product, ProductOption
+
+class ProductDetailView(View):
+    def get(self, request, product_id): 
+        try: 
+            product         = Product.objects.get(id=product_id)
+            product_options = ProductOption.objects.get(id=product_options)
+            total_price     = str(int(product_options.options_price) + int(product.price))
+
+            options = [{
+                'option_colors': product_options.option_colors,
+                'option_size'  : product_options.option_sizes,
+                'option_price' : product_options.options_price
+            }for product_options in ProductOption.filter(product_id=product_id)]
+
+            result = {
+                'id'                : product.id,
+                'name'              : product.name,
+                'price'             : total_price,
+                'image_url'         : product.product_options_images,
+                'options'           : options,
+                'fit_materials_care': product.fit_materials_care,
+                'is_new'            : product.is_new,
+                'is_bestseller'     : product.is_bestseller,
+                'summer_clothes'    : product.summer_clothes
+            }
+            return JsonResponse({'result' : result}, status=200)
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+        except Product.DoesNotExist:
+            return JsonResponse({'message' : 'PRODUCT_NOT_FOUND'}, status=400)
+
--- luluisher/users/models.py
+++ luluisher/users/models.py
@@ -1,15 +1,9 @@
 from django.db       import models
 from products.models import Product
 
+from core           import TimeStampModel
 
 # Create your models here.
-
-class TimeStampModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        abstract = True
 
 class User(TimeStampModel):
     name            = models.CharField(max_length=100)
