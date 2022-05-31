357bf1606a7e158ca13a126b4d327a83cd2341d7
DDinoBox <97079695+DDinoBox@users.noreply.github.com>
2022-05-31 12:05:51 +0900

    Update urls.py

--- luluisher/products/urls.py
+++ luluisher/products/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from products.views import productListView, MenuListView
 
 urlpatterns = [
-    path('/productList', productListView.as_view()),
     path('/menu', MenuListView.as_view()),
     ]
